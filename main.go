e6835b7cfd55bb684b77286487715b83ddb2fd6f
ecunal <ecunal@gmail.com>
2015-09-08 11:43:22 +0300

    Pull and fetch options added, walker func wrapped

--- main.go
+++ main.go
@@ -11,34 +11,58 @@ import (
 )
 
 // TODO verbose option
-// TODO fetch
+
+var branch string
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "gits"
 	app.Usage = "check git status"
 	app.Action = func(c *cli.Context) {
-		filepath.Walk(".", check)
+		filepath.Walk(".", walker(status))
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "fetch",
+			Aliases: []string{"f"},
+			Action: func(c *cli.Context) {
+				filepath.Walk(".", walker(fetch))
+			},
+		},
+		{
+			Name:    "pull",
+			Aliases: []string{"p"},
+			Action: func(c *cli.Context) {
+				branch = c.Args().First()
+				filepath.Walk(".", walker(pull))
+			},
+		},
 	}
 
 	app.Run(os.Args)
 }
 
-func check(path string, info os.FileInfo, err error) error {
-	if err != nil {
-		return nil
+func walker(fn filepath.WalkFunc) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if !info.IsDir() {
+			return nil
+		}
+
+		if !isGit(path) {
+			return nil
+		}
+
+		fmt.Println("* " + path)
+		return fn(path, info, err)
 	}
+}
 
-	if !info.IsDir() {
-		return nil
-	}
-
-	if !isGit(path) {
-		return nil
-	}
-
-	fmt.Println("* " + path)
-
+func status(path string, info os.FileInfo, err error) error {
 	// branch
 	// git rev-parse --abbrev-ref HEAD
 	cmdb := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
@@ -57,6 +81,35 @@ func check(path string, info os.FileInfo, err error) error {
 	} else {
 		fmt.Println(clean)
 	}
+
+	return filepath.SkipDir
+}
+
+func fetch(path string, info os.FileInfo, err error) error {
+	// git fetch
+	cmd := exec.Command("git", "fetch")
+	cmd.Dir = path
+	res, _ := cmd.CombinedOutput()
+	clean := strings.TrimSpace(string(res))
+	if clean == "" {
+		fmt.Println("Already up-to-date.")
+	} else {
+		fmt.Println(clean)
+	}
+
+	return filepath.SkipDir
+}
+
+func pull(path string, info os.FileInfo, err error) error {
+	// git pull
+	args := []string{"pull"}
+	if branch != "" {
+		args = append(args, "origin", branch)
+	}
+	cmd := exec.Command("git", args...)
+	cmd.Dir = path
+	res, _ := cmd.CombinedOutput()
+	fmt.Println(strings.TrimSpace(string(res)))
 
 	return filepath.SkipDir
 }
