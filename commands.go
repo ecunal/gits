b4739c73619e41b278186619e36bc51b71cf8313
ecunal <ecunal@gmail.com>
2015-11-23 15:47:31 +0200

    common executeTrimmed function for shared code

--- commands.go
+++ commands.go
@@ -25,10 +25,7 @@ func status(path string, info os.FileInfo, err error) error {
 
 // fetch prunes the remote branches with "-p" option.
 func fetch(path string, info os.FileInfo, err error) error {
-	cmd := exec.Command("git", "fetch", "-p")
-	cmd.Dir = path
-	res, _ := cmd.CombinedOutput()
-	clean := strings.TrimSpace(string(res))
+	clean := executeTrimmed(path, "git", "fetch", "-p")
 	if clean == "" {
 		fmt.Println("Already up-to-date.")
 	} else {
@@ -43,39 +40,34 @@ func pull(path string, info os.FileInfo, err error) error {
 		branch = currentBranch(path)
 	}
 
-	cmd := exec.Command("git", "pull", "origin", branch)
-	cmd.Dir = path
-	res, _ := cmd.CombinedOutput()
-	fmt.Println(strings.TrimSpace(string(res)))
-
+	fmt.Println(executeTrimmed(path, "git", "pull", "origin", branch))
 	return filepath.SkipDir
 }
 
 func checkout(path string, info os.FileInfo, err error) error {
-	cmd := exec.Command("git", "checkout", branch)
-	cmd.Dir = path
-	res, _ := cmd.CombinedOutput()
-	fmt.Println(strings.TrimSpace(string(res)))
-
+	fmt.Println(executeTrimmed(path, "git", "checkout", branch))
 	return filepath.SkipDir
 }
 
 func currentBranch(path string) string {
-	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
-	cmd.Dir = path
-	res, _ := cmd.CombinedOutput()
-	return strings.TrimSpace(string(res))
+	return executeTrimmed(path, "git", "rev-parse", "--abbrev-ref", "HEAD")
 }
 
-func isGit(dir string) bool {
-	cmd := exec.Command("git", "rev-parse", "--is-inside-work-tree")
-	cmd.Dir = dir
+func isGit(path string) bool {
+	res := executeTrimmed(path, "git", "rev-parse", "--is-inside-work-tree")
+	return res == "true"
+}
+
+func executeTrimmed(path, command string, arg ...string) string {
+	cmd := exec.Command(command, arg...)
+	cmd.Dir = path
+
 	res, err := cmd.CombinedOutput()
 	if err != nil {
-		return false
+		return ""
 	}
 
-	return strings.TrimSpace(string(res)) == "true"
+	return strings.TrimSpace(string(res))
 }
 
 func isWhitespace(s string) bool {
